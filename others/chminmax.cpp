dded47aed3ed69d7136f31e258c88f925a40717f
tsushiy <28962399+tsushiy@users.noreply.github.com>
2019-09-21 19:53:32 +0900

    Clean

--- others/chminmax.cpp
+++
@@ -1,2 +0,0 @@
-template<class T, class U>inline bool chmax(T &a, const U &b) { if(a<b){ a=b; return 1; } return 0; }
-template<class T, class U>inline bool chmin(T &a, const U &b) { if(b<a){ a=b; return 1; } return 0; }
