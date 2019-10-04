d0b7e1f9dd65cfa5ebe92745cfe969dc18783286
tsushiy <28962399+tsushiy@users.noreply.github.com>
2019-10-04 22:54:27 +0900

    Add Comb

---
+++ math/comb.hpp
@@ -0,0 +1,16 @@
+template<unsigned int N = 50>
+struct Comb {
+  long long c[N+1][N+1];
+
+  constexpr Comb() noexcept : c() {
+    for (size_t i = 0; i <= N; ++i) {
+      for (size_t j = 0; j <= N; ++j) {
+        if (j == 0 or j == i) c[i][j] = 1LL;
+        else c[i][j] = c[i-1][j-1] + c[i-1][j];
+      }
+    }
+  }
+  constexpr long long comb(unsigned int n, unsigned int k) const noexcept {
+    return c[n][k];
+  }
+};
