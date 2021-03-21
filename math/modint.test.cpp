dfde9ae720eccd0ef2dcc87798404d298ee05627
tsushiy <28962399+tsushiy@users.noreply.github.com>
2021-03-21 16:38:25 +0900

    Modify Modint

--- math/modint.test.cpp
+++ math/modint.test.cpp
@@ -5,20 +5,25 @@
 int main() {
   constexpr long long MOD = 1000000007;
 
-  static_assert(Mint<MOD>(2) == Mint<MOD>(2), "");
-  static_assert(Mint<MOD>(2) != Mint<MOD>(3), "");
-
-  static_assert(Mint<MOD>(-2) == Mint<MOD>(MOD - 2), "");
-  static_assert(Mint<MOD>(100 * MOD + 2) == Mint<MOD>(2), "");
+  static_assert((Mint<MOD>(2) += Mint<MOD>(2)) == Mint<MOD>(4), "");
+  static_assert((Mint<MOD>(2) -= Mint<MOD>(1)) == Mint<MOD>(1), "");
+  static_assert((Mint<MOD>(2) *= Mint<MOD>(4)) == Mint<MOD>(8), "");
+  static_assert((Mint<MOD>(8) /= Mint<MOD>(4)) == Mint<MOD>(2), "");
 
   static_assert(Mint<MOD>(2) + Mint<MOD>(3) == Mint<MOD>(5), "");
-  static_assert(Mint<MOD>(2) - Mint<MOD>(5) == Mint<MOD>(MOD - 3), "");
+  static_assert(Mint<MOD>(2) - Mint<MOD>(5) == Mint<MOD>(-3), "");
   static_assert(Mint<MOD>(7) * Mint<MOD>(8) == Mint<MOD>(56), "");
   static_assert(Mint<MOD>(16) / Mint<MOD>(2) == Mint<MOD>(8), "");
 
   static_assert(Mint<MOD>(2).pow(4) == Mint<MOD>(16), "");
-  static_assert(Mint<MOD>(1).inv() * 1 == 1, "");
-  static_assert(Mint<MOD>(7).inv() * 7 == 1, "");
+  static_assert(Mint<MOD>(1).inv() * Mint<MOD>(1) == Mint<MOD>(1), "");
+  static_assert(Mint<MOD>(7).inv() * Mint<MOD>(7) == Mint<MOD>(1), "");
+
+  static_assert(Mint<MOD>(2) == Mint<MOD>(2), "");
+  static_assert(Mint<MOD>(2) != Mint<MOD>(3), "");
+
+  static_assert(Mint<MOD>(-2) == Mint<MOD>(MOD - 2), "");
+  static_assert(Mint<MOD>(MOD + 2) == Mint<MOD>(2), "");
 
   cout << "Hello World" << endl;
 
